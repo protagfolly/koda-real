6908e89b68e7142a8c280d458cbdae719ee6cd06
Jake <jake@fulmine.xyz>
2025-07-11 16:58:19 +0200

    fix: copy and paste error (I promise I made this)

--- config/lorekeeper/ext-tracker/multiple_trait_subtypes.php
+++ config/lorekeeper/ext-tracker/multiple_trait_subtypes.php
@@ -3,7 +3,7 @@
 return [
     'wiki_key' => 'Multiple_Trait_Subtypes',
     'creators' => json_encode([
-        'Uri' => 'https://github.com/Draconizations/',
+        'Fulmn' => 'https://github.com/Draconizations/',
     ]),
     'version'  => '1.0.0',
 ];
