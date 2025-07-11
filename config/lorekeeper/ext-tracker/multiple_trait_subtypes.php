04bf27d67d6a25527b9cb291368cd4164f5a82be
Jake <jake@fulmine.xyz>
2025-07-11 11:49:04 +0200

    add extension tracker file

---
+++ config/lorekeeper/ext-tracker/multiple_trait_subtypes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'wiki_key' => 'Multiple_Trait_Subtypes',
+    'creators' => json_encode([
+        'Uri' => 'https://github.com/Draconizations/',
+    ]),
+    'version'  => '1.0.0',
+];
